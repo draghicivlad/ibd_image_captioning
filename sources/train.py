5131a0e93ac395718da03fbec6355d131d9b75d3
Andu199 <aboldisor@yahoo.com>
2025-01-12 14:21:17 +0200

    Added data aug and early stopping

--- sources/train.py
+++ sources/train.py
@@ -3,32 +3,44 @@ import os
 
 import lightning as L
 import yaml
-from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 from lightning.pytorch.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader
 from torchvision.models import ResNet18_Weights, ResNet34_Weights, ResNet50_Weights
+from torchvision.transforms import v2
 
 from sources.dataset import Flickr30k, make_collater
 from sources.model import Baseline
 from sources.utils import create_vocab_flickr30k
 
 
-def get_transform(config):
+def get_transform(config, train=False):
     if config["encoder"]["name"].lower() == "resnet18":
-        return ResNet18_Weights.IMAGENET1K_V1.transforms()
+        transforms = ResNet18_Weights.IMAGENET1K_V1.transforms()
     elif config["encoder"]["name"].lower() == "resnet34":
-        return ResNet34_Weights.IMAGENET1K_V1.transforms()
+        transforms = ResNet34_Weights.IMAGENET1K_V1.transforms()
     elif config["encoder"]["name"].lower() == "resnet50":
-        return ResNet50_Weights.IMAGENET1K_V1.transforms()
+        transforms = ResNet50_Weights.IMAGENET1K_V1.transforms()
     else:
         raise ValueError("Expected: resnet18, resnet34, resnet50; Got: " + config["encoder"]["name"])
+
+    if "use_data_augmentation" in config and config["use_data_augmentation"] and train:
+        print("DATA AUGMENTATION IS USED!")
+        transforms = v2.Compose([
+            v2.RandomHorizontalFlip(p=0.5),
+            v2.RandomRotation(10),
+            v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms
+        ])
+
+    return transforms
 
 
 def load_datasets(config):
     vocab = create_vocab_flickr30k(config["data_root_path"])
 
     train_dataset = Flickr30k(vocab=vocab, root_path=config["data_root_path"],
-                              transforms=get_transform(config), split="train")
+                              transforms=get_transform(config, train=True), split="train")
     val_dataset = Flickr30k(vocab=vocab, root_path=config["data_root_path"],
                             transforms=get_transform(config), split="val")
     collate_fn = make_collater(vocab)
@@ -66,15 +78,19 @@ def init_logging(name):
     logger = TensorBoardLogger(logger_path)
 
     # CALLBACKS
-    callbacks = []
-    callbacks.append(
+    callbacks = [
         ModelCheckpoint(
             monitor="val_loss",
             dirpath=param_path,
             filename='model-{epoch:02d}',
             mode="min",
+        ),
+        EarlyStopping(
+            monitor="val_loss",
+            mode="min",
+            patience=5,
         )
-    )
+    ]
 
     return logger, callbacks
 
